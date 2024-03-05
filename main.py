fc3ed3780ecd0a4cd47288e7bbb5cfab147e31e2
Underslash12 <stew_smith@hotmail.com>
2024-03-05 13:24:57 -0800

    Main now creates data folder if it doesn't exist

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import os
+
 from bot import DiscordBot
 
 if __name__ == "__main__":
+    # make sure the data folder exists since it isn't tracked 
+    if not os.path.exists("./data"): 
+        os.makedirs("./data") 
+
     DiscordBot.initialize()
