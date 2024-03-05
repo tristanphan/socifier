28dfefc2a082069adf3647a9d59a721d8d689b6b
Underslash12 <stew_smith@hotmail.com>
2024-03-05 13:21:26 -0800

    Fixed serialization not working with non-str keys

--- serializer.py
+++ serializer.py
@@ -8,7 +8,7 @@ def dump(obj, filepath):
     Dump a jsonpickle object to a filepath
     """
 
-    data = jsonpickle.encode(obj)
+    data = jsonpickle.encode(obj, keys=True)
     with open(filepath, "w") as f:
         f.write(data)
 
@@ -21,9 +21,9 @@ def load(filepath) -> Optional[object]:
 
     try:
         with open(filepath, "r") as f:
-            obj = jsonpickle.decode(f.read())
+            obj = jsonpickle.decode(f.read(), keys=True)
     except FileNotFoundError:
-        print(f"Couldn't find file specified by the path: {filepath}")
+        print(f"Couldn't load json file specified by the path: {filepath}")
         return None
 
     return obj
